cd47641a477d1ef7d40867118146b896bb73faee
Ankit Saini <nnkitsaini@gmail.com>
2023-10-09 09:24:08 +0530

    support cors

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,8 +1,8 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-function createMap(values: IterableIterator<[string, string]>): {[key: string]: string | string[]} {
-  let params: {[key: string]: string | string[]} = {};
+function createMap(values: IterableIterator<[string, string]>): { [key: string]: string | string[] } {
+  let params: { [key: string]: string | string[] } = {};
   for (const [key, value] of values) {
     if (key in params) {
       let p = params[key];
@@ -15,14 +15,14 @@ function createMap(values: IterableIterator<[string, string]>): {[key: string]: 
       params[key] = value;
     }
   }
-  
+
   return params;
 }
 
 export const GET: RequestHandler = async (event) => {
   let params = createMap(event.url.searchParams.entries());
   let headers = createMap(event.request.headers.entries());
-  let response =  json({
+  let response = json({
     params,
     headers,
     path: event.url.pathname,
@@ -36,7 +36,7 @@ export const POST: RequestHandler = async (event) => {
   let params = createMap(event.url.searchParams.entries());
   let headers = createMap(event.request.headers.entries());
   let body = await event.request.text();
-  let response =  json({
+  let response = json({
     params,
     headers,
     path: event.url.pathname,
@@ -47,19 +47,16 @@ export const POST: RequestHandler = async (event) => {
   return response;
 }
 
+// Support all cors stuff
 export const OPTIONS: RequestHandler = async (event) => {
-  let params = createMap(event.url.searchParams.entries());
-  let headers = createMap(event.request.headers.entries());
-  let body = await event.request.text();
-  let response =  json({
-    params,
-    headers,
-    path: event.url.pathname,
-    body,
-    url: event.url.toString()
-  })
+  const response = new Response();
   response.headers.append('Access-Control-Allow-Origin', "*");
-  response.headers.append('Access-Control-Request-Method', '*')
-  response.headers.append('Access-Control-Request-Headers', '*')
+  response.headers.append('Access-Control-Allow-Methods', "*");
+  response.headers.append('Access-Control-Allow-Headers', "*");
+  response.headers.append('Access-Control-Allow-Credentials', "true");
+  response.headers.append('Access-Control-Max-Age', "86400");
   return response;
+
+
+
 }
